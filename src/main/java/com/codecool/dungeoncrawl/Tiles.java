c49bf1d49ec66b5dd9044381dd1bd2c04d8958a5
904756 <904756@gmail.com>
2022-10-12 19:33:51 +0300

    Changed skeletons

--- src/main/java/com/codecool/dungeoncrawl/Tiles.java
+++ src/main/java/com/codecool/dungeoncrawl/Tiles.java
@@ -27,15 +27,13 @@ public class Tiles {
         tileMap.put("wall", new Tile(19, 11));
         tileMap.put("floor", new Tile(0, 0));
         tileMap.put("player", new Tile(27, 0));
-        tileMap.put("skeleton", new Tile(29, 6));
-        tileMap.put("skeleton1", new Tile(29, 3));
-        tileMap.put("skeleton2", new Tile(29, 1));
+        tileMap.put("skeleton", new Tile(24, 8));
+        tileMap.put("skeleton1", new Tile(26, 8));
+        tileMap.put("skeleton2", new Tile(29, 8));
         tileMap.put("key", new Tile(17 , 23));
         tileMap.put("sword", new Tile(0, 31));
         tileMap.put("open door", new Tile(2, 9));
         tileMap.put("closed door", new Tile(0, 9));
-
-
     }
 
     public static void drawTile(GraphicsContext context, Drawable d, int x, int y) {
